c1be594259cb623c884364f2e802d37de7ef7016
Michael Booth <michael@databooth.com.au>
2024-06-20 16:00:53 +1000

    Prep for deploy

--- proxy.mjs
+++ proxy.mjs
@@ -7,10 +7,26 @@ await initializeAuthProxy({
     authUrl: process.env.PROPEL_TRY_AUTH_URL,
     integrationApiKey: process.env.PROPEL_TRY_API_KEY,
     proxyPort: 8000,
-    urlWhereYourProxyIsRunning: process.env.RENDER_PROXY_URL,
+    urlWhereYourProxyIsRunning: process.env.PROXY_SERVER_URL,
     target: {
         host: process.env.TARGET_HOST,
         port: process.env.TARGET_PORT,
         protocol: process.env.TARGET_PROTOCOL
     },
 });
+
+
+// import { initializeAuthProxy } from '@propelauth/auth-proxy'
+
+// // Replace with your configuration
+// await initializeAuthProxy({
+//     authUrl: "YOUR_AUTH_URL",
+//     integrationApiKey: "YOUR_API_KEY",
+//     proxyPort: 8000,
+//     urlWhereYourProxyIsRunning: 'http://localhost:8000',
+//     target: {
+//         host: 'localhost',
+//         port: 8501,
+//         protocol: 'http:'
+//     },
+// })
